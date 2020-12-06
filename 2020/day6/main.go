4d74fa8910f6bcb8af056fb292fc159e1212b885
Brodie Davis <bdavis9d@gmail.com>
2020-12-06 01:51:28 -0500

    day6 2020

---
+++ 2020/day6/main.go
@@ -0,0 +1,64 @@
+package day6
+
+import "strconv"
+
+func Part1(input []string) string {
+	// store character as int32, since we're only looking for count of unique
+	set := make(map[int32]struct{})
+	count := 0
+	for _, line := range input {
+		if line == "" {
+			count += len(set)
+			set = make(map[int32]struct{})
+			continue
+		}
+		for _, c := range line {
+			set[c] = struct{}{}
+		}
+	}
+	// Don't forget that final group with no trailing empty line!
+	return strconv.Itoa(count + len(set))
+}
+
+func Part2(input []string) string {
+	// store character as int32, since we're only looking for count of unique
+	group := make(map[int32]bool)
+	count := 0
+	for _, line := range input {
+		firstPerson := len(group) == 0
+		person := make(map[int32]bool)
+
+		if line == "" {
+			for _, q := range group {
+				if q {
+					count += 1
+				}
+			}
+
+			group = make(map[int32]bool)
+			continue
+		}
+
+		for _, c := range line {
+			person[c] = true
+
+			// First person dictates the questions everyone else can answer yes too
+			if firstPerson {
+				group[c] = true
+			}
+		}
+
+		for question, groupVal := range group {
+			_, ok := person[question]
+			// group has agreed thus far && person answered yes to question
+			group[question] = groupVal && ok
+		}
+	}
+	// Don't forget that final group with no trailing empty line!
+	for _, q := range group {
+		if q {
+			count += 1
+		}
+	}
+	return strconv.Itoa(count)
+}
--- structs.go
+++ structs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/rudedog9d/advent-of-code/2020/day1"
 	"github.com/rudedog9d/advent-of-code/2020/day2"
+	"github.com/rudedog9d/advent-of-code/2020/day6"
 )
 
 // Map of the 25 days for accessing certain functions from CLI
@@ -23,6 +24,10 @@ func Build2020() ChallengeYear {
 	Year2020["day2"] = ChallengeDay{
 		Part1: day2.Part1,
 		Part2: day2.Part2,
+	}
+	Year2020["day6"] = ChallengeDay{
+		Part1: day6.Part1,
+		Part2: day6.Part2,
 	}
 
 	return Year2020
