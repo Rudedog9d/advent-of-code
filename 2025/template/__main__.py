8ce025533d255b50834a6d470085772db24406d6
Brodie Davis <bdavis@c2games.org>
2024-12-05 12:16:09 -0500

    2025 python template

---
+++ 2025/template/__main__.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+data = [
+    "MMMSXXMASM"
+    "MSAMXMSMSA"
+    "AMXSXMAAMM"
+    "MSAMASMSMX"
+    "XMASAMXAMM"
+    "XXAMMXXAMA"
+    "SMSMSASXSS"
+    "SAXAMASAAA"
+    "MAMMMXMMMM"
+    "MXMXAXMASX"
+]
+# data = [line.strip() for line in open(Path(Path(__file__).parent, 'input.txt'))]
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+print(f"part1: {part1()}")
+print(f"part2: {part2()}")
